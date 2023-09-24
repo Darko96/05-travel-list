15619f29070d10ae351975ff9a54a67267eab003
Darko Mijatovic <darkomijatovic2022@gmail.com>
2023-09-24 13:05:58 +0200

    Moving Components Into Separate Files

---
+++ src/Form.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+export default function Form({ onAddItems }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { id: Date.now, description, quantity, package: false };
+    onAddItems(newItem);
+
+    setDescription("");
+    setQuantity("");
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select
+        value={quantity}
+        onChange={(event) => setQuantity(Number(event.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
+      ></input>
+      <button>Add</button>
+    </form>
+  );
+}
